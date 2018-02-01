68dadcd81c847eae9d88eafeaed96c2adbd991b3
prashantvicky <prashant.vicky@gmail.com>
2018-02-01 16:16:40 +0530

    Update fixed.sh

--- app/fixed.sh
+++ app/fixed.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 cat /app/app/fluentd_config_test1.yaml | sed  's/:{{namespace}}/:\\"default\\"/g'   |sed 's/\/{{namespace}}/\/default/g' | sed 's/{{namespace}}/\"default\"/g' | kubectl apply -f -
